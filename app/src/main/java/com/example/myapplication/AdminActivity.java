a22612751c044b0bccf2d47364d0039011d53f3c
Manny5500 <marvshaye@gmail.com>
2023-09-30 23:20:36 +0800

    May Fragment na siya

--- app/src/main/java/com/example/myapplication/AdminActivity.java
+++ app/src/main/java/com/example/myapplication/AdminActivity.java
@@ -1,14 +1,67 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AdminActivity extends AppCompatActivity {
-
+    Button dashboard, priority, usermanage,  reports;
+    ImageView logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        dashboard = findViewById(R.id.btnDashboards);
+        priority = findViewById(R.id.btnPriority);
+        usermanage = findViewById(R.id.btnUM);
+        logout = findViewById(R.id.btnAdminLogOut);
+        reports = findViewById(R.id.btnReports);
+
+        dashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new fragmentDashboard());
+            }
+        });
+        priority.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new fragmentPriority());
+            }
+        });
+        usermanage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new fragmentUM());
+            }
+        });
+        reports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new fragmentReports());
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  intent = new Intent(AdminActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager =  getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayoutAdmin,fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/myapplication/PersonnelActivity.java
+++ app/src/main/java/com/example/myapplication/PersonnelActivity.java
@@ -1,14 +1,64 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PersonnelActivity extends AppCompatActivity {
-
+    Button personnelProfile, addData, manageData,syncData, logOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personnel);
+
+        personnelProfile=findViewById(R.id.btnProfile);
+        addData = findViewById(R.id.btnAddData);
+        manageData = findViewById(R.id.btnManageData);
+        syncData = findViewById(R.id.btnSyncData);
+        logOut = findViewById(R.id.btnLogout);
+        personnelProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new fragmentPersonnelProfile());
+            }
+        });
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new fradAddData());
+            }
+        });
+        manageData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new fragmentManageData());
+            }
+        });
+        syncData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new fragmentSyncData());
+            }
+        });
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PersonnelActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager =  getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayoutPersonnel,fragment);
+        fragmentTransaction.commit();
     }
 }
